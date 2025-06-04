171b68b3047df341087aa8157e27a5143b7fbe80
tguennec <159457126+tguennec@users.noreply.github.com>
2025-06-04 11:54:08 +0200

    DemoGithubApplication.java

--- src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -2,6 +2,9 @@ package com.example.demo_github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class DemoGithubApplication {
